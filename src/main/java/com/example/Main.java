1c9612ed8d382a5c7fa71b62b5119466b029e226
Areej504 <areej.abdelatif@gmail.com>
2024-11-04 19:41:06 -0500

    Initial commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniShopifySysc4806Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniShopifySysc4806Application.class, args);
+	}
+
+}
