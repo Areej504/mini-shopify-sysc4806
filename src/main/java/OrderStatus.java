06e4029045341a8c18a1262cf8704b2615db4d70
warda <73768064+1ws10@users.noreply.github.com>
2024-11-09 09:27:42 -0500

    commit test cases

--- src/main/java/OrderStatus.java
+++ src/main/java/OrderStatus.java
@@ -5,7 +5,9 @@ public enum OrderStatus {
     CANCELED("Order was canceled"),
     RETURN_REQUESTED("Return has been requested by customer"),
     RETURNED("Order was returned by customer"),
-    REFUNDED("Customer has been refunded");
+    REFUNDED("Customer has been refunded"),
+    COMPLETED("Customer completed order"),
+    CANCELLED("Customer cancelled order"),;
 
     private final String description;
 
