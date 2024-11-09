618e13dd90b5bc1dbb4bace203027a31f2469db2
Areej504 <areej.abdelatif@gmail.com>
2024-11-08 23:20:29 -0500

    miscellaneous

--- src/main/java/com/example/controller/MerchantController.java
+++ src/main/java/com/example/controller/MerchantController.java
@@ -4,10 +4,7 @@ import com.example.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,22 +24,24 @@ public class MerchantController {
 
     //Merchant creation form submission
     @PostMapping("/create-merchant")
-    public String createMerchant(@ModelAttribute Merchant merchant) {
+    public String createMerchant(@ModelAttribute Merchant merchant, Model model) {
         merchantRepository.save(merchant);
         return "redirect:/merchant?merchantId=" + merchant.getMerchantId(); // Redirect to merchant dashboard
     }
 
     // Mapping for the merchant button to open merchantView1.html
-    @GetMapping("/merchant")
-    public String openMerchantHomeScreen() {
+    @RequestMapping("/merchant")
+    public String openMerchantHomeScreen(@RequestParam Long merchantId, Model model) {
         return "merchantView1";
     }
 
     @GetMapping("/create-shop")
     public String showCreateShopForm(Model model) {
-        // Add Category enum values to the model
-        model.addAttribute("categories", Category.values());
+
+        // Add the merchant and shop to the model
         model.addAttribute("shop", new Shop());
+        model.addAttribute("categories", Category.values());  // Assuming you have categories to show
+
         return "createShop";
     }
 
@@ -50,15 +49,17 @@ public class MerchantController {
     public String createShop(@RequestParam String shopName, @ModelAttribute Shop shop, Model model) {
         // Add the shop name to the model for use in the view
         model.addAttribute("shopName", shopName);
+
         // Save the shop object
         shopRepository.save(shop);
 
-        // Redirect to "manage-stores" with query parameters indicating success
         return "redirect:/manage-stores?created=true&shopName=" + shopName;
     }
 
     @GetMapping("/manage-stores")
-    public String openManageStores(@RequestParam Long merchantId, Model model){
+    public String openManageStores(Model model){
+        //TODO: Figure out a way to pass the merchantId to these pages so its not hard-coded
+        Long merchantId = 1L;
         Merchant merchant = merchantRepository.findById(merchantId)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid merchant Id:" + merchantId));
 
