2e03522fd655bb23cde713b9dc2811e28162e405
Areej504 <areej.abdelatif@gmail.com>
2024-11-09 12:35:25 -0500

    Added ProductController and set up addProduct modal view

---
+++ src/main/java/com/example/controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.example.controller;
+
+import com.example.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ProductController {
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private ShopRepository shopRepository;
+
+    @GetMapping("/merchantShop/{shopId}")
+    public String viewMerchantShop(@PathVariable Long shopId, Model model) {
+        Shop shop = shopRepository.findById(shopId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid shop Id"));
+        model.addAttribute("shop", shop); // Add shop details to the model
+
+        model.addAttribute("product", new Product()); // Add Product model to Thymeleaf
+        model.addAttribute("products", productRepository.findAll()); // Fetch all products
+        return "merchantShop";
+    }
+
+    @PostMapping("/merchantShop/{shopId}")
+    public String addProduct(@ModelAttribute Product product) {
+        // save Product
+        productRepository.save(product);
+        return "merchantShop";
+    }
+
+}
