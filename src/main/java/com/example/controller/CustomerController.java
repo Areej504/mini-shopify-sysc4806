88057fa1becabb7cd495533584f46ff986be7c90
Areej504 <areej.abdelatif@gmail.com>
2024-11-08 21:05:05 -0500

    persisting shop, merchant, and customer objects in Controller

--- src/main/java/com/example/controller/CustomerController.java
+++ src/main/java/com/example/controller/CustomerController.java
@@ -1,14 +1,24 @@
 package com.example.controller;
 
+import com.example.model.Customer;
+import com.example.model.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CustomerController {
+    @Autowired
+    private CustomerRepository customerRepository;
 
     // Mapping for the shopper button to open customerShop.html
     @GetMapping("/shopper")
-    public String openCustomerShop() {
-        return "customerScreen"; // This points to customerShop.html in the templates folder
+    public String openCustomerScreen() {
+        //TODO: Add a login screen so this isn't hard-coded!
+        Customer customer = new Customer();
+        customer.setName("Areej");
+        customer.setEmail("areej@shopify.com");
+        customerRepository.save(customer);
+        return "customerScreen"; // This points to customerScreen.html in the templates folder
     }
 }
--- src/main/java/com/example/controller/MerchantController.java
+++ src/main/java/com/example/controller/MerchantController.java
@@ -1,31 +1,48 @@
 package com.example.controller;
 
-import com.example.model.Category;
+import com.example.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MerchantController {
-    // Mapping for the shopper button to open customerShop.html
+    @Autowired
+    private MerchantRepository merchantRepository;
+    @Autowired
+    private ShopRepository shopRepository;
+
+    // Mapping for the merchant button to open merchantView1.html
     @GetMapping("/merchant")
     public String openMerchantHomeScreen() {
+        //TODO: Add a login screen so this isn't hard-coded!
+        Merchant merchant = new Merchant();
+        merchant.setName("Rama");
+        merchant.setEmail("rama@shopify.com");
+        merchantRepository.save(merchant);
         return "merchantView1";
     }
 
     @GetMapping("/create-shop")
-    public String openCreateShop(Model model) {
+    public String showCreateShopForm(Model model) {
         // Add Category enum values to the model
         model.addAttribute("categories", Category.values());
+        model.addAttribute("shop", new Shop());
         return "createShop";
     }
 
     @PostMapping("/create-shop")
-    public String createShop(@RequestParam("shopName") String shopName, Model model) {
-        // Add the shop name as a parameter to be accessed in the view
+    public String createShop(@RequestParam String shopName, @ModelAttribute Shop shop, Model model) {
+        // Add the shop name to the model for use in the view
         model.addAttribute("shopName", shopName);
+        // Save the shop object
+        shopRepository.save(shop);
+
+        // Redirect to "manage-stores" with query parameters indicating success
         return "redirect:/manage-stores?created=true&shopName=" + shopName;
     }
 
